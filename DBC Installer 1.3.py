8bdebc45869f7c5fb5262450a1c8398a949df05d
Meit Sant <97303250+MT-276@users.noreply.github.com>
2022-09-21 15:44:35 +0530

    Updated DBC Installer 1.2.py

---
+++ DBC Installer 1.3.py
@@ -0,0 +1,39 @@
+#-------------------------------------------------------------------------------
+# Name:        DBC Installations
+# Purpose:     Install necesarry components needed for DBC
+#
+# Author:      MS Productions
+# Created:     19/09/2022
+# Copyright:   (c) MS Productions 2022
+#-------------------------------------------------------------------------------
+import time ,sys , subprocess, os
+import multiprocessing as mp
+
+def run_cmd(att):
+    cmd = "pip install "+att
+    os.system(cmd)
+
+dependencies = [
+    ('pynput'),
+    ('pyautogui'),
+    ('datetime'),
+    ('selenium'),
+    ('openpyxl'),
+    ('tkinter'),
+    ('numpy'),
+    ('pystray'),
+    ('struct'),
+    ('Pillow'),
+
+]
+process_list = []
+
+for i in dependencies:
+    process_list.append(mp.Process(target=run_cmd, args=(i,)))
+
+if __name__ == '__main__':
+    for process in process_list:
+      process.start()
+
+    for process in process_list:
+      process.join()
