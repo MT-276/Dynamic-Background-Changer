84b72225651aa9d17118a827978990fe12cbcc7f
Meit Sant <97303250+MT-276@users.noreply.github.com>
2022-09-21 15:41:06 +0530

    Updated DBC Installer v1.1

--- DBC Installer v1.0.py
+++ DBC Installer v1.0.py
@@ -1,6 +1,6 @@
 #-------------------------------------------------------------------------------
-# Name:        DBC Installations
-# Purpose:     Install necesarry components needed for DBC
+# Name:        DBC Instalations
+# Purpose:     Install necesarry components needer for DBC
 #
 # Author:      MS Productions
 # Created:     19/09/2022
@@ -18,31 +18,49 @@ print("[INFO] Running script -")
 print("[INFO] Installing modules :")
 start = time.perf_counter()
 
-dependencies = [
-    ('pynput'),
-    ('pyautogui'),
-    ('datetime'),
-    ('selenium'),
-    ('openpyxl'),
-    ('tkinter'),
-    ('numpy'),
-    ('pystray'),
-    ('struct'),
-    ('Pillow'),
+p1 = mp.Process(target=run_cmd,args=("pynput",))
+p2 = mp.Process(target=run_cmd,args=("pyautogui",))
+p3 = mp.Process(target=run_cmd,args=("datetime",))
+p4 = mp.Process(target=run_cmd,args=("selenium",))
+p5 = mp.Process(target=run_cmd,args=("openpyxl",))
+p6 = mp.Process(target=run_cmd,args=("tkinter",))
+p7 = mp.Process(target=run_cmd,args=("numpy",))
+p8 = mp.Process(target=run_cmd,args=("pystray",))
+p9 = mp.Process(target=run_cmd,args=("struct",))
+p10 = mp.Process(target=run_cmd,args=("Pillow",))
 
-]
-process_list = []
-
-for i in dependencies:
-    process_list.append(mp.Process(target=run_cmd, args=(i,)))
 if __name__ == '__main__':
-    for process in process_list:
-      process.start()
+    p1.start()
+    print("p1 started")
+    p2.start()
+    print("p2 started")
+    p3.start()
+    print("p3 started")
+    p4.start()
+    print("p4 started")
+    p5.start()
+    print("p5 started")
+    p6.start()
+    print("p6 started")
+    p7.start()
+    print("p7 started")
+    p8.start()
+    print("p8 started")
+    p9.start()
+    print("p9 started")
+    p10.start()
+    print("p10 started")
 
-    for process in process_list:
-      process.join()
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
+    p5.join()
+    p6.join()
+    p7.join()
+    p8.join()
+    p9.join()
+    p1.join()
 
 finish = time.perf_counter()
 print(f'Finished in {round(finish-start, 2)} second(s)')
-
-
